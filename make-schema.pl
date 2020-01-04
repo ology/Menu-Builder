75bca4d0d0cf1a4f55c3f33d808ca65453c71f86
Gene Boggs <gene@ology.net>
2020-01-03 22:01:23 -0800

    Automatically configure for DBIx::Class::Schema::Config use

--- make-schema.pl
+++ make-schema.pl
@@ -5,11 +5,14 @@ use warnings;
 use DBIx::Class::Schema::Loader qw/ make_schema_at /;
 
 my $dsn  = 'dbi:SQLite:dbname=menu-builder.db';
-my $user = '';
-my $pass = '';
+my $user = undef;
+my $pass = undef;
 
 make_schema_at(
-    'MenuBuilder::Schema',
-    { debug => 1, dump_directory => './lib', },
+    'MenuBuilder::Schema', {
+        debug             => 1,
+        dump_directory    => './lib',
+        schema_components => [qw(Schema::Config)],
+    },
     [ $dsn, $user, $pass, ],
 );
