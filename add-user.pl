3b27601bc6bfa9333ee6f1aa86a30369a7c3f14f
Gene Boggs <gene@ology.net>
2020-01-03 21:48:08 -0800

    Initial commit

---
+++ add-user.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+use lib 'lib';
+use MenuBuilder::Schema;
+
+use Mojolicious::Lite;
+
+my ( $name, $pass ) = @ARGV;
+
+plugin 'bcrypt';
+
+my $password = app->bcrypt($pass);
+
+my $schema = MenuBuilder::Schema->connect('auth');
+
+$schema->resultset('Account')->create({ name => $name, password => $password });
