24455d2e378a17a7c25a589a166b3b8d396cfa27
Gene Boggs <gene@ology.net>
2020-07-02 06:07:08 -0700

    Whitespace!

--- lib/Mojolicious/Plugin/Helper.pm
+++ lib/Mojolicious/Plugin/Helper.pm
@@ -4,25 +4,25 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Schema;
 
 sub register {
-    my ( $self, $app ) = @_;
+    my ($self, $app) = @_;
 
-    $app->helper( schema => sub {
+    $app->helper(schema => sub {
         my ($c) = @_;
-        return state $schema = Schema->connect( $c->config('database'), '', '' );
-    } );
+        return state $schema = Schema->connect($c->config('database'), '', '');
+    });
 
     $app->helper(rs => sub {
         return shift->schema->resultset(@_);
     });
 
-    $app->helper( auth => sub {
-        my ( $c, $user, $pass ) = @_;
+    $app->helper(auth => sub {
+        my ($c, $user, $pass) = @_;
 
         my $result = $c->schema->resultset('Account')->search({ username => $user })->first;
 
         return $result
             if $result && $result->check_password($pass);
-    } );
+    });
 
 }
 
